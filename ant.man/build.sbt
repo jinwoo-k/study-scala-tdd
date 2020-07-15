eb6b80653c6c00bd92e633f6490e25561cecacda
ttlqudan <ttlqudan@gmail.com>
2020-07-16 08:03:56 +0900

    # 5장 정리 및 privatemethod 테스트

--- ant.man/build.sbt
+++ ant.man/build.sbt
@@ -4,4 +4,6 @@ version := "0.1"
 
 scalaVersion := "2.11.8"
 
-libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.6" % "test"
+libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.6" % "test" // 3.2.0
+libraryDependencies +="info.cukes" %% "cucumber-scala" % "1.2.4" % "test"
+libraryDependencies +="info.cukes" % "cucumber-junit" % "1.2.4" % "test"
---
+++ ant.man/src/test/scala/com/packt/ch5/BaseConversionSteps.scala
@@ -0,0 +1,18 @@
+package com.packt.ch5
+
+class BaseConversionSteps extends BaseSteps {
+  var baseFrom: Number = null
+  var baseTo: Number = null
+  Given("""^I have a (binary|decimal) number (\d+)$"""){
+    (x:String, baseFromNumber:String) =>
+      baseFrom = if (x=="binary") Binary(baseFromNumber) else
+        Decimal(baseFromNumber)
+  }
+  When("""^I convert it to (binary|decimal) using the BaseConversion utility$"""){ (x:String) =>
+    baseTo = if(x=="binary") BaseConversion.decimalToBinary(baseFrom) else
+      BaseConversion.bianryToDecimal(baseFrom)
+  }
+  Then("""^I get back a (binary|decimal) number (\d+)$"""){ (x:String, expected: String) =>
+    baseTo.number shouldBe expected
+  }
+}
